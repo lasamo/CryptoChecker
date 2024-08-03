e9ff12e0cc9213baa529e1187c36bb0146f1b4ce
lasamo <layla-sm@protonmail.com>
2024-08-03 13:17:26 +0200

    Added changes to improve data readability, main

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Required for session management
@@ -31,11 +32,29 @@ def submit():
     try:
         response = requests.get('https://public.chainalysis.com/api/v1/address/{}'.format(address), headers={'X-API-KEY': f'{api_key}'})
         response.raise_for_status()
-        data = response.json()
-        # Add address or other relevant data to the response
-        return render_template('index.html', data=data, address=address)
+        datas = response.json()["identifications"]
+        if datas:
+            for data in datas:
+                if data["category"] == "sanctions":
+                    description = data["description"]
+                    name = data["name"]
+                    url = data["url"]
+        # return render_template('index.html', data=data, address=address)
+        return render_template('index.html', address = address, data = data, description=description, name = name, url = url)
     except requests.RequestException as e:
         return render_template('index.html', error="Error fetching data: " + str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# @app.route('/index', methods=['GET', 'POST'])
+# def index():
+#     response_text = ""
+#     if request.method == 'POST':
+#         address = request.form['address']
+#         # Placeholder for the actual API request using the address
+#         # response = requests.get(f'http://api.example.com/data?address={address}&apikey={api_key}')
+#         # response_text = response.text  # This should be the text you want to display
+#         response_text = f"Received address: {address}"  # Simulated response
+
+#     return render_template('index.html', response_text=response_text)
